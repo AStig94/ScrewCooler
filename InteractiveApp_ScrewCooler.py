eda505a926baa5fbfa14db1f9b2da48202399414
AStig94 <60739899+AStig94@users.noreply.github.com>
2025-07-03 13:51:00 +0200

    Update InteractiveApp_ScrewCooler.py

--- InteractiveApp_ScrewCooler.py
+++ InteractiveApp_ScrewCooler.py
@@ -10,7 +10,7 @@ from scipy.optimize import root_scalar
 # ctrl+c to abort terminal..
 
 # ------------------- Setup -------------------
-st.set_page_config(layout="wide")
+st.set_page_config(page_title="Biochar Screw Cooler",layout="wide")
 st.title("Biochar Screw Cooler")
 st.markdown("Adjust the parameters to simulate the cooling screw behavior.")
 
